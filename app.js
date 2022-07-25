a242673107238d4d87478e5332aed98851c30155
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-25 08:45:45 -0700

    updated and installed sequelize

--- app.js
+++ app.js
@@ -6,5 +6,17 @@ const sequelize = new Sequelize({
 }); //holds Sequelize instance you can intact with 
 // async IIFE
 (async () => { //defines an asynchronous function. 
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to the database successful!');
+    } catch (error) {
+      console.log('Error connecting to database', error);
+    }
+})();
 
+// Movie model
+class Movie extends Sequelize.Model {}
+
+(async () => {
+  
 })();
