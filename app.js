6dbee9da5a3ed8681bdd130ffbec0cdbb522c344
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-25 08:33:16 -0700

    error when I install sequelize

---
+++ app.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'movies.db'
+}); //holds Sequelize instance you can intact with 
+// async IIFE
+(async () => { //defines an asynchronous function. 
+
+})();
