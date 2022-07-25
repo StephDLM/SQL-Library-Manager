6a35b83112670645d58552f9f9be6fcf892171fb
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-25 15:09:07 -0700

    defining the modal

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'movies.db'
-}); //holds Sequelize instance you can intact with 
-// async IIFE
-(async () => { //defines an asynchronous function. 
-    try {
-        await sequelize.authenticate();
-        console.log('Connection to the database successful!');
-    } catch (error) {
-      console.log('Error connecting to database', error);
-    }
-})();
-
-// Movie model
-class Movie extends Sequelize.Model {}
+const db = require('./db');
+const { Movie } = db.models;
 
 (async () => {
-  
+  await db.sequelize.sync({ force: true });
+
+  try {
+    const movie = await Movie.create({
+      title: 'Hereditary'
+    });
+    console.log(movie.toJSON());
+
+    const movie2 = await Movie.create({
+      title: 'Get Out'
+    });
+    console.log(movie2.toJSON());
+
+  } catch (error) {
+    console.error('Error connecting to the database: ', error);
+  }
 })();
