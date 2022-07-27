851de843195a0b57b6dfd2723076fa509258a7f6
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-26 20:46:43 -0700

    renamed users file to books, and redirected the router in index.js to books.js

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const Book = require(".../models").Book;
+const Book = require("../models").Book;
 
-/* GET home page. */
-router.get('/', async(req, res, next) => {
-  //res.render('index', { title: 'Express' });
-    const books = await Book.findAll();
-    res.render("index", { books, title: "New Book" });
-    console.log( books.map(book => book.toJSON()) );
+//redirect into books file
+router.get('/', (req, res, next) => {
+    res.redirect('/books')
    });
   
-
+//res.json--
 module.exports = router;
