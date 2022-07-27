2fa6c1ad46ad4dbdbc7608a816dc7b2df5344f1d
StephDLM <106278655+StephDLM@users.noreply.github.com>
2022-07-27 12:34:53 -0700

    created routes (not updated)

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -55,5 +55,37 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+//pass an object as a parameter to next
+//render error page 404 by sending response to the client, setting up the response status to 404, and render not-found view
+app.use ((req,res, next) => {
+    const err = new Error('not-found');
+    err.status = 404;
+    err.message = "This web page can't be located";
+    console.log("This web page can't be located", err);
+    next(err);
+});
+
+//render global error handler
+app.use((err, req, res, next) => {
+    // setting locals with error property
+    if (err){
+        if (err.status === 404){
+            res.status(404).render('not-found', { err });
+                } else {
+            err.status = 500;
+            err.message = "Oops! Something went wrong with the server.";
+            console.log('Global error handler called',err)
+            res.status(err.status || 500).render('error', {err} )
+        }
+    }
+  });
+
+//start server
+app.listen(3001, () => {
+    console.log('The application is running on localhost:3000!')
+});
+
+
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -13,4 +13,46 @@ router.get('/', async(req, res, next) => {
     res.render("index", { books, title: "Book" });
     console.log( books.map(book => book.toJSON()) );
    });
+
+//get /books - Shows the full list of books
+router.get('/books', async(req, res, next) => {
+    const books = await Book.findAll();
+    res.render("index", { books, title: "Book" });
+    console.log( books.map(book => book.toJSON()) );
+   });
+//get /books/new - Shows the create new book form
+router.get('/books/new', async(req, res, next) => {
+    const books = await Book.findAll();
+    res.render("index", { books, title: "Book" });
+    console.log( books.map(book => book.toJSON()) );
+   });
+
+//post /books/new - Posts a new book to the database
+router.post('/books/new', async(req, res, next) => {
+  const books = await Book.findAll();
+  res.render("index", { books, title: "Book" });
+  console.log( books.map(book => book.toJSON()) );
+ });
+
+//get /books/:id - Shows book detail form
+router.get('/books/:id', async(req, res, next) => {
+  const books = await Book.findAll();
+  res.render("index", { books, title: "Book" });
+  console.log( books.map(book => book.toJSON()) );
+ });
+
+//post /books/:id - Updates book info in the database
+router.post('/books/:id', async(req, res, next) => {
+  const books = await Book.findAll();
+  res.render("index", { books, title: "Book" });
+  console.log( books.map(book => book.toJSON()) );
+ });
+
+//post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting
+router.post('/books/:id/delete', async(req, res, next) => {
+  const books = await Book.findAll();
+  res.render("index", { books, title: "Book" });
+  console.log( books.map(book => book.toJSON()) );
+ });
+
 module.exports = router;
